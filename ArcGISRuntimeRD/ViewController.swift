8cd0c72537be73548f94d2d8c9cb414c7f914a4b
Alexandra <alexandra.lifa@routematch.com>
2017-05-05 19:00:05 -0400

    Display's user location

--- ArcGISRuntimeRD/ViewController.swift
+++ ArcGISRuntimeRD/ViewController.swift
@@ -11,13 +11,26 @@ import Foundation
 import ArcGIS
 
 class ViewController: UIViewController {
-
     @IBOutlet weak var mapView: AGSMapView!
+    var locatorTask: AGSLocatorTask!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Display a map using the ArcGIS Online imagery basemap service
-        self.mapView.map = AGSMap(basemapType: .imageryWithLabelsVector, latitude: 33.7490, longitude: -84.3880, levelOfDetail: 17)
-    }
+        self.mapView.map = AGSMap(basemapType: .imageryWithLabelsVector,  latitude: 33.7490, longitude: -84.3880, levelOfDetail: 14)
+        self.mapView.locationDisplay.showPingAnimationSymbol = true
+        self.mapView.locationDisplay.start{ (error: Error?) -> Void in
+            if let error = error {
+                UIAlertView(title: "Error", message: error.localizedDescription, delegate: nil, cancelButtonTitle: "Ok").show()
+            }
+            else {
+                print("Location Display Started")
+            }
+        }
+        
+        
+        }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
